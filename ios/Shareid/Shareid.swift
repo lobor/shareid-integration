14ff6fa083da7092eb1ba6d44617f98ba4d87db2
lobor <lionel.bertrand@paymium.com>
2024-11-15 06:44:35 +0100

    first

---
+++ ios/Shareid/Shareid.swift
@@ -0,0 +1,37 @@
+//
+//  Shareid.swift
+//  AwesomeProject
+//
+//  Created by Lionel Bertrand on 15/11/2024.
+//
+
+import Foundation
+import shareid_sdk_ios
+
+// @objc(Shareid)
+// public class Shareid {
+
+//     // private let axeptioEventListener = AxeptioEventListener()
+
+//     // override init() {
+//     //     super.init()
+
+//     // }
+
+//     // deinit {
+//     // }
+
+
+//     @objc(mount:withUrl:withToken:withId:)
+//     func mount(
+//         url: String,
+//         token: String,
+//         processorId: String
+//     ) -> Void {
+//         // ShareIDFlow.shared.start(modf: ShareIDAuthenticationMode.onboarding, baseURL: url, token: token)
+//         // resolve(nil)
+//     }
+
+
+// }
+
